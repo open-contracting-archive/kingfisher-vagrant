360a7cd1c7efa2bea1927da09e0d60ce0d4b6d62
David Raznick <kindly@gmail.com>
2018-03-29 10:00:15 +0100

    add setup.py to make paths correct

--- bin/cli.py
+++ bin/cli.py
@@ -1,6 +1,5 @@
 import sys
 from os import path
-sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import ocdsdata.sources.taiwan
 
 
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ocdsdata',
+      version='0.0',
+      description='Get ocds data',
+      author='Open Contracting',
+      url='',
+      packages=['ocdsdata', 'ocdsdata.sources'],
+     )
