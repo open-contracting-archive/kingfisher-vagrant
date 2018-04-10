270c4bfa9ca0f7f68500d43316b02de0fecb8c96
James Baster <james.baster@opendataservices.coop>
2018-04-10 16:53:57 +0100

    Catch the right exception

--- ocdsdata/database.py
+++ ocdsdata/database.py
@@ -14,7 +14,7 @@ try:
     password = pgpasslib.getpass(host, port, user, dbname)
 
     database_uri = 'postgresql://{}:{}@{}/{}'.format(user, password, host, dbname)
-except FileNotFoundError:
+except pgpasslib.FileNotFound:
     database_uri = 'postgresql://{}:{}@{}/{}'.format(user, 'ocdsdata', host, dbname)
 
 DB_URI = os.environ.get('DB_URI', database_uri)
