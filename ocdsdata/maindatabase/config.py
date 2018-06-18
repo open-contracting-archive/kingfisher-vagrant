c6294bbe183c8eabe43f6fe856e3ddea9bfc0f2e
Sebastian Oliva <code@sebastianoliva.com>
2018-06-18 09:02:46 -0600

    pep8 fixes

--- ocdsdata/maindatabase/config.py
+++ ocdsdata/maindatabase/config.py
@@ -3,14 +3,14 @@ import pgpasslib
 import configparser
 
 config = configparser.ConfigParser()
-## User level config file.
+# User level config file.
 config.read('~/.config/ocdsdata/config.ini')
 
 # Loads database details or defaults
 host = config.get('DBHOST', 'HOSTNAME', fallback='localhost')
 port = config.get('DBHOST', 'PORT', fallback='5432')
-user =  config.get('DBHOST', 'USERNAME', fallback='ocdsdata')
-dbname =  config.get('DBHOST', 'DBNAME', fallback='ocdsdata')
+user = config.get('DBHOST', 'USERNAME', fallback='ocdsdata')
+dbname = config.get('DBHOST', 'DBNAME', fallback='ocdsdata')
 
 try:
     password = pgpasslib.getpass(host, port, user, dbname)
@@ -18,5 +18,5 @@ try:
 except pgpasslib.FileNotFound:
     database_uri = 'postgresql://{}:{}@{}/{}'.format(user, 'ocdsdata', host, dbname)
 
-## Overwrites if DB_URI is specified.
+# Overwrites if DB_URI is specified.
 DB_URI = os.environ.get('DB_URI', database_uri)
