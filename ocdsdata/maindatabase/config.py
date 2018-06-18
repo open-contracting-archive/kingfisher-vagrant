4f9d194820e580aed0e77cdb7a9c6b407fcfe5db
Sebastian Oliva <code@sebastianoliva.com>
2018-06-17 22:50:41 -0600

    Loads config parser file before setting default values for DBURI gen.

--- ocdsdata/maindatabase/config.py
+++ ocdsdata/maindatabase/config.py
@@ -1,19 +1,22 @@
 import os
 import pgpasslib
+import configparser
 
-# Default database details
-#
-#
-host = 'localhost'
-port = '5432'
-user = 'ocdsdata'
-dbname = 'ocdsdata'
+config = configparser.ConfigParser()
+## User level config file.
+config.read('~/.config/ocdsdata/config.ini')
+
+# Loads database details or defaults
+host = config.get('HOSTNAME', 'localhost')
+port = config.get('PORT', '5432')
+user =  config.get('USERNAME','ocdsdata')
+dbname =  config.get('DBNAME','ocdsdata')
 
 try:
     password = pgpasslib.getpass(host, port, user, dbname)
-
     database_uri = 'postgresql://{}:{}@{}/{}'.format(user, password, host, dbname)
 except pgpasslib.FileNotFound:
     database_uri = 'postgresql://{}:{}@{}/{}'.format(user, 'ocdsdata', host, dbname)
 
+## Overwrites if DB_URI is specified.
 DB_URI = os.environ.get('DB_URI', database_uri)
