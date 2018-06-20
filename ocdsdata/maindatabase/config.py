87bdc73141e58ea8040668397a410d1d52fd122b
Sebastian Oliva <code@sebastianoliva.com>
2018-06-20 08:19:38 -0600

    more coverage on cases for dburi

--- ocdsdata/maindatabase/config.py
+++ ocdsdata/maindatabase/config.py
@@ -1,10 +1,12 @@
 import os
+import sys
 import pgpasslib
 import configparser
 
 config = configparser.ConfigParser()
 # User level config file.
 config.read(os.path.expanduser('~/.config/ocdsdata/config.ini'))
+
 
 # Loads database details or defaults
 host = config.get('DBHOST', 'HOSTNAME', fallback='localhost')
@@ -13,11 +15,27 @@ user = config.get('DBHOST', 'USERNAME', fallback='ocdsdata')
 dbname = config.get('DBHOST', 'DBNAME', fallback='ocdsdata')
 dbpass = config.get('DBHOST', 'PASSWORD', fallback='ocdsdata')
 
+
+def __gen_dburi(user, password, host, port, dbname):
+    return 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, dbname)
+
+
 try:
-    password = pgpasslib.getpass(host, port, user, dbname)
-    database_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, dbname)
+    fetched_pass = pgpasslib.getpass(host, port, user, dbname)
+    password = fetched_pass if fetched_pass else dbpass
+    database_uri = __gen_dburi(user, password, host, port, dbname)
 except pgpasslib.FileNotFound:
-    database_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, dbpass, host, port, dbname)
+    # Fail silently when no files found.
+    password = dbpass
+    database_uri = __gen_dburi(user, password, host, port, dbname)
+except pgpasslib.InvalidPermissions:
+    print("Your pgpass file has the wrong permissions, for your safety this file will be ignored. Please fix the permissions and try again.")
+    password = dbpass
+    database_uri = __gen_dburi(user, password, host, port, dbname)
+except pgpasslib.PgPassException:
+    print("Unexpected error:", sys.exc_info()[0])
+    password = dbpass
+    database_uri = __gen_dburi(user, password, host, port, dbname)
 
 # Overwrites if DB_URI is specified.
 DB_URI = os.environ.get('DB_URI', database_uri)
