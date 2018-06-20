2c484d78d3d71713c21e29b120a5bced53a607b8
Sebastian Oliva <code@sebastianoliva.com>
2018-06-20 07:34:37 -0600

    pass to default

--- ocdsdata/maindatabase/config.py
+++ ocdsdata/maindatabase/config.py
@@ -11,12 +11,13 @@ host = config.get('DBHOST', 'HOSTNAME', fallback='localhost')
 port = config.get('DBHOST', 'PORT', fallback='5432')
 user = config.get('DBHOST', 'USERNAME', fallback='ocdsdata')
 dbname = config.get('DBHOST', 'DBNAME', fallback='ocdsdata')
+dbpass = config.get('DBHOST', 'PASSWORD', fallback='ocdsdata')
 
 try:
     password = pgpasslib.getpass(host, port, user, dbname)
     database_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, dbname)
 except pgpasslib.FileNotFound:
-    database_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, 'ocdsdata', host, port, dbname)
+    database_uri = 'postgresql://{}:{}@{}:{}/{}'.format(user, dbpass, host, port, dbname)
 
 # Overwrites if DB_URI is specified.
 DB_URI = os.environ.get('DB_URI', database_uri)
