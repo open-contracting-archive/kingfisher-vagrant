04abeb5d1d35348d0ab4a8402c80d6cfd58f6484
Sebastian Oliva <code@sebastianoliva.com>
2018-06-21 18:45:41 -0600

    if not uri then check for config file

--- ocdsdata/maindatabase/config.py
+++ ocdsdata/maindatabase/config.py
@@ -13,35 +13,35 @@ if (len(read_files) == 0 and not env_db_uri):
     print("There are no config files nor DB_URI, therefore we cannot start.")
     quit(-1)
 
-
-# Loads database details or defaults
-host = config.get('DBHOST', 'HOSTNAME')
-port = config.get('DBHOST', 'PORT')
-user = config.get('DBHOST', 'USERNAME')
-dbname = config.get('DBHOST', 'DBNAME')
-dbpass = config.get('DBHOST', 'PASSWORD')
-
-
-def __gen_dburi(user, password, host, port, dbname):
-    return 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, dbname)
+if not env_db_uri:
+    # Loads database details or defaults
+    host = config.get('DBHOST', 'HOSTNAME')
+    port = config.get('DBHOST', 'PORT')
+    user = config.get('DBHOST', 'USERNAME')
+    dbname = config.get('DBHOST', 'DBNAME')
+    dbpass = config.get('DBHOST', 'PASSWORD')
 
 
-try:
-    fetched_pass = pgpasslib.getpass(host, port, user, dbname)
-    password = fetched_pass if fetched_pass else dbpass
-    database_uri = __gen_dburi(user, password, host, port, dbname)
-except pgpasslib.FileNotFound:
-    # Fail silently when no files found.
-    password = dbpass
-    database_uri = __gen_dburi(user, password, host, port, dbname)
-except pgpasslib.InvalidPermissions:
-    print("Your pgpass file has the wrong permissions, for your safety this file will be ignored. Please fix the permissions and try again.")
-    password = dbpass
-    database_uri = __gen_dburi(user, password, host, port, dbname)
-except pgpasslib.PgPassException:
-    print("Unexpected error:", sys.exc_info()[0])
-    password = dbpass
-    database_uri = __gen_dburi(user, password, host, port, dbname)
+    def __gen_dburi(user, password, host, port, dbname):
+        return 'postgresql://{}:{}@{}:{}/{}'.format(user, password, host, port, dbname)
+
+
+    try:
+        fetched_pass = pgpasslib.getpass(host, port, user, dbname)
+        password = fetched_pass if fetched_pass else dbpass
+        database_uri = __gen_dburi(user, password, host, port, dbname)
+    except pgpasslib.FileNotFound:
+        # Fail silently when no files found.
+        password = dbpass
+        database_uri = __gen_dburi(user, password, host, port, dbname)
+    except pgpasslib.InvalidPermissions:
+        print("Your pgpass file has the wrong permissions, for your safety this file will be ignored. Please fix the permissions and try again.")
+        password = dbpass
+        database_uri = __gen_dburi(user, password, host, port, dbname)
+    except pgpasslib.PgPassException:
+        print("Unexpected error:", sys.exc_info()[0])
+        password = dbpass
+        database_uri = __gen_dburi(user, password, host, port, dbname)
 
 # Overwrites if DB_URI is specified.
 DB_URI = env_db_uri or database_uri
