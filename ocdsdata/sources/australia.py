7fa245b815dd08c04db5efca0c9e8b0d8868159c
James Baster <james.baster@opendataservices.coop>
2018-07-05 16:37:10 +0100

    Australia: make first pages "meta" to avoid duplication in the database https://github.com/open-contracting/ocdsdata/issues/145

--- ocdsdata/sources/australia.py
+++ ocdsdata/sources/australia.py
@@ -19,7 +19,7 @@ class AustraliaSource(Source):
             out.append({
                 'url': url % r,
                 'filename': 'type-%s-page-1-.json' % r,
-                'data_type': 'release_package',
+                'data_type': 'meta',
                 'priority': 10,
             })
 
@@ -27,54 +27,51 @@ class AustraliaSource(Source):
 
     # @rate_limited(1)
     def save_url(self, filename, data, file_path):
-        link = 'links'
-        if data['data_type'] == 'release_package':
+        errors = save_content(data['url'], file_path)
+        if errors:
+            return [], errors
 
-            errors = save_content(data['url'], file_path)
-            if errors:
-                return [], errors
+        additional = []
 
-            additional = []
+        if data['data_type'] == 'meta':
 
-            if filename.startswith('type'):
+            with open(file_path) as f:
+                json_data = json.load(f)
 
-                with open(file_path) as f:
-                    json_data = json.load(f)
+            page = int(filename.split('-')[3])
+            type = filename.split('-')[1]
+            if 'links' in json_data and 'next' in json_data['links'] and (not self.sample or page < 3):
+                page += 1
+                additional.append({
+                    'url': json_data['links']['next'],
+                    'filename': 'type-%s-page-%d-.json' % (type, page),
+                    'data_type': 'meta',
+                    'priority': 10,
+                })
 
-                page = int(filename.split('-')[3])
-                type = filename.split('-')[1]
-                if link in json_data and 'next' in json_data[link] and (not self.sample or page < 3):
-                    page += 1
-                    additional.append({
-                        'url': json_data[link]['next'],
-                        'filename': 'type-%s-page-%d-.json' % (type, page),
-                        'data_type': 'release_package',
-                        'priority': 10,
-                    })
+            count = 0
+            for release in json_data['releases']:
+                if not self.sample or count < 3:
+                    stage_urls = []
+                    if type == 'planning':
+                        uuid = release['tender']['plannedProcurementUUID']
+                        stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.planning.view'
+                                          '&PlannedProcurementUUID=%s' % uuid)
+                    if type == 'tender':
+                        uuid = release['tender']['RFTUUID']
+                        stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.tender.view&RFTUUID=%s' % uuid)
+                    if type == 'contract':
+                        for award in release['awards']:
+                            uuid = award['CNUUID']
+                            stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=%s'
+                                              % uuid)
+                    count += 1
+                    for url in stage_urls:
+                        additional.append({
+                            'url': url,
+                            'filename': 'packages-%s.json' % hashlib.md5(url.encode('utf-8')).hexdigest(),
+                            'data_type': 'release_package',
+                            'priority': 1,
+                        })
 
-                count = 0
-                for release in json_data['releases']:
-                    if not self.sample or count < 3:
-                        stage_urls = []
-                        if type == 'planning':
-                            uuid = release['tender']['plannedProcurementUUID']
-                            stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.planning.view'
-                                              '&PlannedProcurementUUID=%s' % uuid)
-                        if type == 'tender':
-                            uuid = release['tender']['RFTUUID']
-                            stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.tender.view&RFTUUID=%s' % uuid)
-                        if type == 'contract':
-                            for award in release['awards']:
-                                uuid = award['CNUUID']
-                                stage_urls.append('https://tenders.nsw.gov.au/?event=public.api.contract.view&CNUUID=%s'
-                                                  % uuid)
-                        count += 1
-                        for url in stage_urls:
-                            additional.append({
-                                'url': url,
-                                'filename': 'packages-%s.json' % hashlib.md5(url.encode('utf-8')).hexdigest(),
-                                'data_type': 'release_package',
-                                'priority': 1,
-                            })
-
-            return additional, []
+        return additional, []
