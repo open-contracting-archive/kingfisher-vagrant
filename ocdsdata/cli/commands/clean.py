95b5e68cdf18d2d41b43c7b213309f64533c0404
Sebastian Oliva <code@sebastianoliva.com>
2018-07-26 18:48:10 -0600

    stub for clean subcommand

---
+++ ocdsdata/cli/commands/clean.py
@@ -0,0 +1,17 @@
+import os
+
+import ocdsdata.cli.commands.base
+import ocdsdata.sources_util
+
+
+class CleanCLICommand(ocdsdata.cli.commands.base.CLICommand):
+    command = 'clean'
+
+    def __init__(self):
+        self.sources = ocdsdata.sources_util.gather_sources()
+
+    def configure_subparser(self, subparser):
+        subparser.add_argument("source", help="Wipe this source data")
+
+    def run_command(self, args):
+        pass
