463c4ab351808403e789374ef21696270dc8abf5
Sebastian Oliva <code@sebastianoliva.com>
2018-08-02 10:43:59 -0600

    no misleading line on readme

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ from distutils.core import setup
 here = os.path.abspath(os.path.dirname(__file__))
 
 # Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
